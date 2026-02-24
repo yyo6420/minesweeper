54af10c737d028e483f0dac0857e54d80d3aa0be
yyo6420 <yyo6420@gmail.com>
2026-02-24 15:41:29 +0200

    board feature is ready

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import { useState } from "react";
 import "./styles/App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="page">
+      <h1 className="pageTitle">מצא את כל המוקשים</h1>
+      <h2 className="counterText">
+        מספר מוקשים שהתגלו:
+      </h2>
+    </div>
+  );
 }
 
 export default App;
